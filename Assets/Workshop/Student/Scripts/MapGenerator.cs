309035b0580e4fc929846f0f1776ade881041133
EkaiUnix <settawut.pakd@bumail.net>
2026-08-25 16:05:38 +0700

    Update_MapGenerator

--- Assets/Workshop/Student/Scripts/MapGenerator.cs
+++ Assets/Workshop/Student/Scripts/MapGenerator.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using UnityEngine;
 
 namespace Workshop.Student
@@ -19,24 +19,46 @@ namespace Workshop.Student
         };
 
         // 1. declare Players variable
+        public GameObject[] playerTiles;
 
         // 7. declare Exit variable 
+        public GameObject exitTile;
 
 
         public void Start()
         {
             // 1. random player at the position <0, 0> map
 
+            if (playerTiles != null && playerTiles.Length > 0)
+            {
+                // กำหนดพิกัดตายตัว เช่น X = 0, Y = 0
+                int x_Player = 0;
+                int y_Player = 0;
+
+                int r_Player = UnityEngine.Random.Range(0, playerTiles.Length);
+                Instantiate(playerTiles[r_Player], new Vector2(x_Player, y_Player), Quaternion.identity);
+                
+            }
+
             // 2. create obstacles
 
-            // 3. create floor
+            if (wallTiles != null && wallTiles.Length > 0)
+            {
+                int x_obstacles = columns / 2;
+                for (int y_obstacles = 0; y_obstacles < rows / 2; y_obstacles++)
+                {
+                    int r_obstacles = UnityEngine.Random.Range(0, wallTiles.Length);
+                    Instantiate(wallTiles[r_obstacles], new Vector2(x_obstacles, y_obstacles), Quaternion.identity);
+                }
+            }
 
-            for (int y = 0; y < rows; y++)
+            // 3. create floor
+            for (int y = 0; y < columns; y++)
             {
                 for (int x = 0; x < columns; x++)
                 {
                     int r = UnityEngine.Random.Range(0, floorTiles.Length);
-                    GameObject tile = Instantiate(floorTiles[r], new Vector2(x, y), Quaternion.identity);
+                    GameObject tile = Instantiate(floorTiles[0], new Vector2(x, y), Quaternion.identity);
                     tile.name = "Floor" + x + "_" + y;
                 }
             }
@@ -90,6 +112,7 @@ namespace Workshop.Student
             }
 
             // 7. place exit
+            Instantiate(exitTile, new Vector2(columns - 1, rows - 1), Quaternion.identity);
 
         }
     }
